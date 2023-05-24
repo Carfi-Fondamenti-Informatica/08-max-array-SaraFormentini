2a632723acf512cf74cf6d56428280c4c617d3a7
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-24 19:12:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dimensione=0;
+    cin>>dimensione;
+    float numeri [dimensione];
+    for (int i=0;i<dimensione; i++){
+        cin>>numeri [i];
+    }
+    cout<<array (numeri,dimensione);
+
+    return 0;
 }
