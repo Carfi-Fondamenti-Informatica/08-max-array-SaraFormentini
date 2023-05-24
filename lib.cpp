bdacaa9d938dca73d6fe96b98cf74a223ea4559b
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-24 19:12:57 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+#include "lib.h"
 
+float array (float numeri [],int dimensione){
+    int max =numeri [0];
+    for (int i=0;i<=dimensione;i++){
+        if(max<numeri[i]){
+            max=numeri[i];
+       }
+    }return max;
+}
