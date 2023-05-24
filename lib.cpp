9c69e118c75a7b0c17415f5b81f80543386983b0
SaraFormentini <127684525+SaraFormentini@users.noreply.github.com>
2023-05-24 19:33:03 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,7 +2,7 @@
 
 float array (float numeri [],int dimensione){
     int max =numeri [0];
-    for (int i=0;i<=dimensione;i++){
+    for (int i=0;i<dimensione;i++){
         if(max<numeri[i]){
             max=numeri[i];
        }
